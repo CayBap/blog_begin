d0607321c23fc1bb7b0d23a55309fc671059f6b7
You Ci <youci.thang@gmail.com>
2017-10-05 11:06:28 +0700

    Finished interface bolog

--- blog/apps/controllers/blog.js
+++ blog/apps/controllers/blog.js
@@ -1,7 +1,42 @@
 var express = require("express");
 var router = express.Router();
+var post_md = require("../models/post");
 
 router.get("/",function(req,res){
-    res.json({"message":"this is blog page"});
+
+    // res.json({"message":"this is blog page"});
+    var data = post_md.getAllPosts();
+
+    data.then(function(posts){
+        //var posts=posts[0];
+        var result = {
+            posts: posts,
+            error:false
+        }
+        
+        res.render("blog/index.ejs",{data:result});
+    }).catch(function(err){
+        var result = {
+            error:"Could not get posts data"
+        }
+        res.render("blog/index.ejs",{data:result});
+    })
+    //res.render("blog/index.ejs");
 });
+router.get("/post/:id",function(req,res){
+    var data = post_md.getPostById(req.params.id);
+    data.then(function(posts){
+        var post = posts[0];
+        var result = {
+            post:post,
+            error : MSFIDOCredentialAssertion
+        };
+        res.render("blog/post",{data:result});
+    }).catch(function(err){
+        var result ={
+            error:"Could not get post detail"
+        };
+        res.render("blog/post",{data:result});
+    })
+})
 module.exports = router;
--- blog/test.js
+++ blog/test.js
@@ -1,15 +1,11 @@
-let add = (a,b)=>{
-    return new Promise((resolve,reject) =>{
-        setTimeout(function() {
-            if(typeof a != 'number'||typeof b !='number'){
-                return reject(new Error("Loi"));
-            }
-            resolve(a+b);
-        }, 1000);
-    });
-}
-add(1,'').then((resolve)=>{
-    console.log(resolve);
-},(err)=>{
-    console.log(err + '');
-})
+data.then(function(posts){
+    var data = {
+      posts:posts,
+      error : false
+    };
+   // console.log(data);
+    res.render("admin/dashboard",{data:data});
+  })
+  .catch(function(err){
+    res.render("admin/dashboard",{data:{error:"Get Post data is error"}});
+  });
